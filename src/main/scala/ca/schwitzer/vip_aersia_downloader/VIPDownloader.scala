ff6ca85f2e9e470abc33340d8cf3c2099b1ceb35
Mitchell Schwitzer <mitchell@schwitzer.ca>
2016-11-03 19:45:02 -0700

    write sink, fix future return
    the future returns immediately on downloadAll, causing onComplete to
    execute immediately in Runner

--- src/main/scala/ca/schwitzer/vip_aersia_downloader/VIPDownloader.scala
+++ src/main/scala/ca/schwitzer/vip_aersia_downloader/VIPDownloader.scala
@@ -1,20 +1,23 @@
 package ca.schwitzer.vip_aersia_downloader
 
-import java.nio.file.Path
+import java.nio.file.{Path, Paths}
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
 import akka.http.scaladsl._
 import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import akka.util.ByteString
 import com.google.inject.Inject
 import com.typesafe.config.Config
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 import scala.xml.{Elem, XML}
 
 trait VIPDownloader {
-  def downloadAll(savePath: Path): Unit //TODO: replace
+  def downloadAll(savePath: Path): Future[Done]
 }
 
 class VIPDownloaderImpl @Inject()(implicit config: Config,
@@ -40,9 +43,15 @@ class VIPDownloaderImpl @Inject()(implicit config: Config,
     .via(httpFlow)
     .flatMapConcat(_.entity.dataBytes.reduce(_ concat _))
 
-  override def downloadAll(savePath: Path): Unit = {
+  def writeSink(savePath: Path) = Sink.foreach[(String, ByteString)] {
+    case (filename, data) =>
+      Source.single(data).runWith(FileIO.toPath(Paths.get(savePath + "/" + filename)))
+      println(s"Done downloading $filename.")
+  }
+
+  override def downloadAll(savePath: Path): Future[Done] = {
     //limited at 5000, tracks shouldnt exceed this many unless the author of the site adds a TON more..
-    xmlSource.via(filenameFlow).limit(5000).runWith(Sink.seq).map { filenames =>
+    xmlSource.via(filenameFlow).limit(5000).runWith(Sink.seq).flatMap { filenames =>
       //group into a stream of streams 80 tracks long, as http flows will stop processing after ~100 elems
       val fileStreams = Source(filenames)
         .grouped(80)
@@ -50,7 +59,7 @@ class VIPDownloaderImpl @Inject()(implicit config: Config,
 
       //map over each stream in the main stream. each stream will run in parallel
       fileStreams.map { stream =>
-        stream.zip(stream.via(downloadTracksFlow))
+        stream.zip(stream.via(downloadTracksFlow)).runWith(writeSink(savePath))
       }.runWith(Sink.ignore)
     }
   }
